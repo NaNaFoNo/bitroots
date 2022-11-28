2b8774f30aa4b3179a23d8a46ec51de6f8cc5585
Andreas Vieten <cryptonari@outlook.com>
2022-11-28 18:24:43 +0100

    add: implement Navbar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,12 @@ import {
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./Logo"
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <Navbar />
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
@@ -34,5 +37,6 @@ export const App = () => (
         </VStack>
       </Grid>
     </Box>
+    <Footer />
   </ChakraProvider>
 )
